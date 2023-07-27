f74592bf16cc43f4913a08dbcd2ea63d9209f8c6
ulbigen <136837666+ulianabi@users.noreply.github.com>
2023-07-27 19:49:40 +0300

    views.py

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 def index(requset):
     return render(requset, 'index.html')
-def top_sellers()
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def advertisement_post(requset):
+    return render(requset, 'advertisement-post.html')
+def register(requset):
+    return render(requset, 'register.html')
+def login(requset):
+    return render(requset, 'login.html')
+def profile(requset):
+    return render(requset, 'profile.html')
