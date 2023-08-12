be1b9dbc18cb428c06f177a847dd2e3f4c61c900
ulbigen <136837666+ulianabi@users.noreply.github.com>
2023-08-13 00:10:13 +0300

    admin.py

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Advertisement
+from django.db.models.query import QuerySet
 
-# Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auctin', 'created_date', 'updated_date']
+    list_filter = ['auctin', 'creted_at', 'price']
+    actions = ['make_action_as_false', 'make_action_as_true']
+    fieldsets = (
+        ('Общие', { # блок 1
+            "fields": (
+                'title', 'description'     # поля блока
+            ),
+        }),
+        ('Финансы', {  # блок 1
+            "fields": (
+                'price', 'auctin'  # поля блока
+            ),
+        }),
+    )
+
+
+
+    @admin.action(description='Убрать возможность торга')
+    def make_action_as_false(self, request, queryset: QuerySet):
+        queryset.update(auctin=False)
+
+    @admin.action(description='Добавить возможность торга')
+    def make_action_as_true(self, request, queryset: QuerySet):
+        queryset.update(auctin=True)
+
+admin.site.register(Advertisement, AdvertisementAdmin)
