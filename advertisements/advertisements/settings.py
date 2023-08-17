1af31249aaf5e93eb325545998d502112955bb09
ulbigen <136837666+ulianabi@users.noreply.github.com>
2023-08-17 21:25:06 +0300

    settings.py

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -122,6 +122,10 @@ STATICFILES_DIRS = [
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media/')
+
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
