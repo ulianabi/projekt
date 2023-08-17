068feb0cf62af2edde80ad21849b280c6bf5c9a5
ulbigen <136837666+ulianabi@users.noreply.github.com>
2023-08-17 21:17:47 +0300

    models.py

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -3,6 +3,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils import timezone
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Advertisement(models.Model):
@@ -12,6 +15,12 @@ class Advertisement(models.Model):
     auctin = models.BooleanField("торг", help_text="Отметьте, если торг уместен", default=False)
     creted_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('изображение', upload_to='advertisements/')
+
+
+
+
 
     @admin.display(description='дата создания')
     def created_date(self):
