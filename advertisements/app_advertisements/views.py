c773ed554bde0d0b0954086c40564f28878f358c
ulbigen <136837666+ulianabi@users.noreply.github.com>
2023-08-17 21:20:28 +0300

    views.py

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Advertisement
 
 def index(requset):
-    return render(requset, 'index.html')
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements}
+    return render(requset, 'index.html', context)
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
